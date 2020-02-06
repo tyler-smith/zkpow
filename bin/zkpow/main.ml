f1c65604cf6cd1b0df1a52e312676edeae3cf1f4
Tyler Smith <tylersmith.me@gmail.com>
2020-02-05 18:29:02 -0800

    cleanup bin, make recursive, produce 10 blocks.

--- bin/zkpow/main.ml
+++ bin/zkpow/main.ml
@@ -35,8 +35,10 @@ let create_step_prover_state prev_state prev_state_hash prev_state_proof state w
   ; prev_hash= prev_state_hash
   ; expected_next_state= state}
 
-let create_wrap_prover_state proof =
-  {Wrap.Prover_state.proof=proof}
+let step_prover_state_zero =
+  let open Tick.Field in
+  let negative_one = negate one in
+  create_step_prover_state negative_one (of_int (hash negative_one)) Tock.Proof.dummy zero (Tock.Keypair.vk wrap_kp)
 
 let next_step_prover_state (state : Transition.Make.Step_base.Prover_state.t) proof =
   let open Tick in
@@ -47,58 +49,40 @@ let next_step_prover_state (state : Transition.Make.Step_base.Prover_state.t) pr
   ; prev_hash= (hash_tick_field state.expected_next_state)
   ; expected_next_state= expected_next_state}
   
-let generate_step_proof step_pk state state_hash =
+let create_wrap_prover_state proof =
+  {Wrap.Prover_state.proof=proof}
+
+let create_step_proof step_pk state state_hash =
   let open Transition.Make in
   let open Tick in
-  prove step_pk (tick_input ()) state Step.main2 state_hash
+  let proof = prove step_pk (tick_input ()) state Step.main2 state_hash in
+  assert (Tick.verify proof step_vk (Step.input ()) state_hash);
+  proof
 
-let generate_wrap_proof hash proof =
+let wrap_proof hash proof =
   let input = Wrap_input.of_tick_field hash in
   let proof =
     Tock.prove
       (Tock.Keypair.pk wrap_kp)
       Wrap.input {Wrap.Prover_state.proof} Wrap.main input
   in
+  assert (Tock.verify proof wrap_vk Wrap.input (Wrap_input.of_tick_field hash));
   proof
 
-let () = 
-  let open Tick.Field in
 
-  let state = create_step_prover_state zero (of_int (hash zero)) Tock.Proof.dummy one (Tock.Keypair.vk wrap_kp) in
-  let proof = generate_step_proof step_pk state (hash_tick_field state.expected_next_state) in
-  printf "Block0 state: %s\nBlock0 hash: %s\nBlock0 proof: %s\n" (to_string state.expected_next_state) (to_string state.prev_hash) (b64_encode_tick_proof proof);
-  assert (Tick.verify proof step_vk (Step.input ()) (hash_tick_field state.expected_next_state));
-
-  let wrap_proof = generate_wrap_proof (hash_tick_field state.expected_next_state) proof in
-  printf "Block0 wrap proof: %s\n\n\n" (b64_encode_tock_proof wrap_proof);
-  assert (Tock.verify wrap_proof wrap_vk Wrap.input (Wrap_input.of_tick_field(hash_tick_field state.expected_next_state)));
-
-  (* let state = next_step_prover_state state wrap_proof in
-  let proof = generate_step_proof step_pk state (hash_tick_field state.expected_next_state) in
-  printf "Block1 state: %s\nBlock1 hash: %s\nBlock1 proof: %s\n" (to_string state.expected_next_state) (to_string state.prev_hash) (b64_encode_tick_proof proof);
-  assert (Tick.verify proof step_vk (Step.input ()) (hash_tick_field state.expected_next_state));
-
-  let wrap_proof = generate_wrap_proof (hash_tick_field state.expected_next_state) proof in
-  printf "Block1 wrap proof: %s\n\n\n" (b64_encode_tock_proof wrap_proof);
-  assert (Tock.verify wrap_proof wrap_vk Wrap.input (Wrap_input.of_tick_field(hash_tick_field state.expected_next_state))); *)
-
-  for i = 1 to 3 do
-    let state = next_step_prover_state state wrap_proof in
-    let proof = generate_step_proof step_pk state (hash_tick_field state.expected_next_state) in
-    printf "Block%d state: %s\nBlock%d hash: %s\nBlock%d proof: %s\n" i (to_string state.expected_next_state) i (to_string state.prev_hash) i (b64_encode_tick_proof proof);
-    assert (Tick.verify proof step_vk (Step.input ()) (hash_tick_field state.expected_next_state));
+let rec create_next_proof (i:int) state wrapped_proof =
+  if i >= 10 then ()
+  else
+    let module Field = Tick.Field in
+    let state = next_step_prover_state state wrapped_proof in
+    let state_hash = (hash_tick_field state.expected_next_state) in
+    let proof = create_step_proof step_pk state state_hash in
+    printf "Block%d state: %s\nBlock%d hash: %s\nBlock%d proof: %s\n" i (Field.to_string state.expected_next_state) i (Field.to_string state_hash) i (b64_encode_tick_proof proof);
   
-    let wrap_proof = generate_wrap_proof (hash_tick_field state.expected_next_state) proof in
-    printf "Block%d wrap proof: %s\n\n\n" i (b64_encode_tock_proof wrap_proof);
-    assert (Tock.verify wrap_proof wrap_vk Wrap.input (Wrap_input.of_tick_field(hash_tick_field state.expected_next_state)));
-  done
+    let wrapped_proof = wrap_proof state_hash proof in
+    printf "Block%d wrap proof: %s\n\n" i (b64_encode_tock_proof wrapped_proof);
 
+    create_next_proof (i+1) state wrapped_proof
 
-  (* let state = next_prover_state state proof in
-  let proof = generate_step_proof step_pk state state.prev_hash in
-  printf "Block0 state: %s\nBlock0 hash: %s\nBlock0 proof: %s\n" (to_string state.expected_next_state) (to_string state.prev_hash) (b64_encode_proof proof); *)
-
-(* let () = 
-  base_hash, base_proof = generate_base()
-  print "Base hash: %s" base_hash
-  print "Base proof: %s" base_proof *)
+let () = 
+  create_next_proof 0 step_prover_state_zero Tock.Proof.dummy
