bd620272ed488ae62d33f4e2ad344ffaeac3fcac
Tyler Smith <tylersmith.me@gmail.com>
2020-02-07 18:23:45 -0800

    REFACTOR: Reorganize repo and start on dep installation.

--- Makefile
+++ Makefile
@@ -4,11 +4,13 @@
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
+CLI_BINARY="src/app/zkpow/main.exe"
+
 build: ## Build all components
-	@dune build -j8 ./bin/zkpow/main.exe
+	@dune build -j8 $(CLI_BINARY)
 
 run: ## Build and execute the main binary
-	@dune exec ./bin/zkpow/main.exe
+	@dune exec $(CLI_BINARY)
 
 install: ## Install all components
 	@dune install 
@@ -24,26 +26,5 @@ clean: ## Clean dune artifacts
 	@dune clean
 
 install_deps: ## Install dependencies from OPAM
-	@opam switch import opam.export --switch zkpow
-
-MYUID = $(shell id -u)
-DOCKERNAME = zkpowbuilder-$(MYUID)
-
-ifeq ($(DUNE_PROFILE),)
-DUNE_PROFILE := dev
-endif
-
-TMPDIR ?= /tmp
-
-ifeq ($(USEDOCKER),TRUE)
- $(info INFO Using Docker Named $(DOCKERNAME))
- WRAP = docker exec -it $(DOCKERNAME)
- WRAPAPP = docker exec --workdir /home/opam/app -t $(DOCKERNAME)
-else
- $(info INFO Not using Docker)
- WRAP =
-endif
-
-
-containerstart: ## Restarts codabuilder
-	@./scripts/container.sh restart
+	@git submodule init
+	@./scripts/setup-opam.sh
---
+++ scripts/setup-opam.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -x # echo on
+set -eu
+
+# Keep compile dirs to avoid recompiles
+export OPAMKEEPBUILDDIR='true'
+export OPAMREUSEBUILDDIR='true'
+export OPAMYES=1
+
+# Set term to xterm if not set
+export TERM=${TERM:-xterm}
+
+# ocaml environment
+eval $(opam config env)
+
+# check for cache'd opam
+SWITCH_BASE='4.07.1'
+SWITCH='zkpow-4.07.1'
+
+
+# opam switch remove $SWITCH || true # temp hack for testing, remove!!!
+
+SWITCH_LIST=$(opam switch list -s)
+
+
+# Check to see if we have explicit switch version
+SWITCH_FOUND=false
+for val in $SWITCH_LIST; do
+  if [ $val == $SWITCH ]; then
+    SWITCH_FOUND=true
+  fi
+done
+
+if [ "$SWITCH_FOUND" = true ]; then
+  opam switch set $SWITCH
+  eval $(opam config env)
+else
+  # Build opam from scratch
+  opam init
+  opam update
+  opam switch create $SWITCH $SWITCH_BASE || true
+  opam switch $SWITCH
+fi
+
+# workaround a permissions problem in rpc_parallel .git
+chmod -R u+rw ~/.opam
+
+opam install -y jbuilder
+
+# Our pins
+# opam pin add src/external/async_kernel
+opam pin add src/external/digestif
+opam pin add src/external/ppx_optcomp
+opam pin add src/external/snarky
+eval $(opam config env)
+
+# All our ocaml packages
+opam switch import src/opam.export
+eval $(opam config env)
+
+# show switch list at end
+echo "opam switch list"
+opam switch list -s
