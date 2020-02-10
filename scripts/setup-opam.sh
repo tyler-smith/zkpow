c42b83cb2eb7745b25665eceb1253ceded9e34d8
Tyler Smith <tylersmith.me@gmail.com>
2020-02-10 13:28:54 -0800

    Try moving digestif pin before import.

--- scripts/setup-opam.sh
+++ scripts/setup-opam.sh
@@ -42,17 +42,15 @@ chmod -R u+rw ~/.opam
 
 opam install -y dune jbuilder
 
+opam pin add src/external/ppx_optcomp
+opam pin add src/external/digestif
+
 # All our ocaml packages
 opam switch import src/opam.export
 eval $(opam config env)
 
 # Our pins
 # opam pin add src/external/async_kernel
-opam pin add src/external/ppx_optcomp
-opam pin add src/external/digestif
 opam pin add src/external/snarky
-eval $(opam config env)
 
-# show switch list at end
-echo "opam switch list"
-opam switch list -s
+eval $(opam config env)
